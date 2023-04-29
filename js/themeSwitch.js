dd6626651f0c6ae77184ec11a357d911d3be07cc
Richard Kužela <richard@kanshen.click>
2023-04-29 11:35:14 +0200

    Create themeSwitch.js

---
+++ js/themeSwitch.js
@@ -0,0 +1,12 @@
+function switchtheme() {
+    const HTML = document.querySelector("html")
+    var themeValue = HTML.getAttribute("theme");
+    
+    if (themeValue = "dark") {
+        HTML.setAttribute("theme", "light");
+        document.cookie = "theme=light; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+    } else if (themeValue = "light") {
+        HTML.setAttribute("theme", "dark");
+        document.cookie = "theme=dark; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+    }
+}
